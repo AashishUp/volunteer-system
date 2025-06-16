3ceacfe4b96055851142d4eb8a9f411f05ff9508
AashishUp <aashishuprety661@gmail.com>
2025-06-16 16:53:40 +0545

    Authorization, login and registration done

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,3 +25,21 @@ exports.registerUser = async (req, res)=>{
     }
 };
 
+exports.loginUser = async (req, res)=>{
+    const { email, password} = req.body;
+    try{
+        const user = await User.findOne({ email });
+        if(!user) return res.status(400).json({ message: 'User not registered'});
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!user) return res.status(400).json({message: 'Invalid credentials'});
+
+        const token = jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET, {expiresIn: '7d',});
+        res.json({
+            message: 'Login Successful',
+            token,
+            user:{id:user._id, name: user.name, email: user.email, role: user.role},
+        });
+    } catch (err){
+        res.status(500).json({message: 'Server error', error: err.message});       
+    }
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require ('./routes/authRoutes');
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.send('API is running.....');
 });
+
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 5000;
 
